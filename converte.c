243ac3080eae1885572d5ce8a562d707fcb247ce
ags1999 <alexandresanson8@gmail.com>
2021-10-08 20:56:21 -0300

    Comentários e variáveis renomeadas

--- converte.c
+++ converte.c
@@ -1,15 +1,16 @@
 /* Alexandre_Sanson 1711450 3WB */
+/* Luana_Durand 1820807 */
 #include <stdlib.h>
 #include "converte.h"
 
 
 union data
     {
-        unsigned int i1;
+        unsigned int valor_int;
         unsigned char vetor[4];
     };
 
-int tamanho(char c1)
+int tamanho(char c1) //Retorna o tamanho em bytes do caractere utf
 {
     int i = 0;
     unsigned char aux = c1;
@@ -26,43 +27,42 @@ int tamanho(char c1)
     return i;
 }
 
-void utf_varint_aux(FILE *arq_saida, int aux, int tm)
+void utf_varint_aux(FILE *arq_saida,unsigned int palavra, int tm) // converte e insere o valor recebido
 {
     
 
-    int v0, v1, v2, v3;
+    int byte0, byte1, byte2, byte3;
     char grupo_7bits;
     union data temp;
-    temp.i1 = aux;
+    temp.valor_int = palavra;
 
 
     switch (tm)
     {
     case 2:
-        v1 = ((temp.vetor[1] - 0xC0));
-        v1 <<= 6;
-        v0 = (temp.vetor[0] - 0x80);
-        temp.i1 =  v0 + v1;
+        byte1 = ((temp.vetor[1] - 0xC0));
+        byte1 <<= 6;
+        byte0 = (temp.vetor[0] - 0x80);
+        temp.valor_int =  byte0 + byte1;
         break;
     
     case 3:
-        v2 = (temp.vetor[2] - 0xE0);
-        v2 <<= 12;
-        v1 = (temp.vetor[1] - 0x80);
-        v1 <<= 6;
-        v0 = (temp.vetor[0] - 0x80);
-        temp.i1 =  v0 + v1 + v2;
+        byte2 = (temp.vetor[2] - 0xE0);
+        byte2 <<= 12;
+        byte1 = (temp.vetor[1] - 0x80);
+        byte1 <<= 6;
+        byte0 = (temp.vetor[0] - 0x80);
+        temp.valor_int =  byte0 + byte1 + byte2;
         break;
 
     case 4:
-        v3 = temp.vetor[3] - 0xF0;
-        v3 <<= 18;
-        v2 = temp.vetor[2] - 0x80;
-        v2 <<= 12;
-        v1 = temp.vetor[1] - 0x80;
-        v1 <<= 6;
-        v0 = (temp.vetor[0] - 0x80);
-        temp.i1 = v0 + v1 + v2 + v3;
+        byte3 <<= 18;
+        byte2 = temp.vetor[2] - 0x80;
+        byte2 <<= 12;
+        byte1 = temp.vetor[1] - 0x80;
+        byte1 <<= 6;
+        byte0 = (temp.vetor[0] - 0x80);
+        temp.valor_int = byte0 + byte1 + byte2 + byte3;
         break;
 
     default:
@@ -77,7 +77,7 @@ void utf_varint_aux(FILE *arq_saida, int aux, int tm)
         grupo_7bits &= 0x7f;
 
 
-        if((temp.i1 >> 7) == 0)
+        if((temp.valor_int >> 7) == 0)
         {
 
             fputc(grupo_7bits, arq_saida);
@@ -89,7 +89,7 @@ void utf_varint_aux(FILE *arq_saida, int aux, int tm)
 
             fputc(grupo_7bits, arq_saida);
         }
-        temp.i1 >>= 7;
+        temp.valor_int >>= 7;
     }
     
     return; 
@@ -99,9 +99,9 @@ void utf_varint_aux(FILE *arq_saida, int aux, int tm)
 int utf_varint(FILE *arq_entrada, FILE *arq_saida)
 {
     int i = 0, tm = 0;
-    unsigned int aux;
-    unsigned char c1;
-
+    unsigned int palavra;
+    unsigned char byte1;
+    //palavra
     while(1)
     {
         if(ferror(arq_entrada))
@@ -115,70 +115,70 @@ int utf_varint(FILE *arq_entrada, FILE *arq_saida)
             fprintf(stderr,"Erro de Escrita\n");
             return -1;
         }
-        aux = 0;
+        palavra = 0;
         i = 0;
-        c1 = fgetc(arq_entrada);
+        byte1 = fgetc(arq_entrada);
         if(feof(arq_entrada))
             break;
 
-        tm = tamanho(c1);
-        aux += c1;
+        tm = tamanho(byte1);
+        palavra += byte1;
         i++;
         while (i < tm)
         {
-            aux <<= 8;
-            aux += fgetc(arq_entrada);
+            palavra <<= 8;
+            palavra += fgetc(arq_entrada);
             i++;
         }
         
 
-        utf_varint_aux(arq_saida, aux, tm); 
+        utf_varint_aux(arq_saida, palavra, tm); 
   
     }
 
     return 0;
 }
 
-unsigned int varint_utf_aux(unsigned char aux[],int tm, FILE *arq_saida)
+unsigned int varint_utf_aux(unsigned char aux[],int tm, FILE *arq_saida) // converte e insere o valor recebido
 {
         
     int i;
     union data temp, insercao;
-    insercao.i1 = 0;
-    temp.i1 = 0;
+    insercao.valor_int = 0;
+    temp.valor_int = 0;
     for(i = 0; i < tm - 1; i++)
     {
-        temp.i1 += (aux[i] - 0x80) << 7*i;
+        temp.valor_int += (aux[i] - 0x80) << 7*i;
     }
-    temp.i1 += aux[i] << 7*i;
+    temp.valor_int += aux[i] << 7*i;
         
 
     
-    if(temp.i1 < 0x80)
+    if(temp.valor_int < 0x80)
     {
         fputc(temp.vetor[0], arq_saida);
     }
-    else if(temp.i1 < 0x800)
+    else if(temp.valor_int < 0x800)
     {
-        insercao.i1 = 0xC080;
+        insercao.valor_int = 0xC080;
 
         insercao.vetor[0] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
         insercao.vetor[1] += temp.vetor[0];
 
         fputc(insercao.vetor[1], arq_saida);
         fputc(insercao.vetor[0], arq_saida);
         
     }
-    else if (temp.i1 < 0x10000)
+    else if (temp.valor_int < 0x10000)
     {
-        insercao.i1 = 0xE08080;
+        insercao.valor_int = 0xE08080;
 
         insercao.vetor[0] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
 
         insercao.vetor[1] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
 
         insercao.vetor[2] += temp.vetor[0];
 
@@ -189,17 +189,17 @@ unsigned int varint_utf_aux(unsigned char aux[],int tm, FILE *arq_saida)
     }
     else
     {
-        insercao.i1 = 0xF0808080;
+        insercao.valor_int = 0xF0808080;
 
         insercao.vetor[0] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
         i = temp.vetor[0] & 0x7F;
 
         insercao.vetor[1] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
 
         insercao.vetor[2] += (temp.vetor[0] & 0x3F);
-        temp.i1 >>= 6;
+        temp.valor_int >>= 6;
 
         insercao.vetor[3] += temp.vetor[0];
 
@@ -218,8 +218,8 @@ unsigned int varint_utf_aux(unsigned char aux[],int tm, FILE *arq_saida)
 int varint_utf(FILE *arq_entrada, FILE *arq_saida)
 {
     int tm;
-    unsigned char c1;
-    unsigned char aux[5] = {0,0,0,0,0};
+    unsigned char byte1;
+    unsigned char palavra[5] = {0,0,0,0,0};
     
     while (1)
     {
@@ -236,24 +236,24 @@ int varint_utf(FILE *arq_entrada, FILE *arq_saida)
         }
         
         tm = 0;
-        c1 = fgetc(arq_entrada);
+        byte1 = fgetc(arq_entrada);
         if(feof(arq_entrada))
             break;
         
         
-        aux[tm] = c1;
+        palavra[tm] = byte1;
         tm++;
-        while (c1 >> 7)
+        while (byte1 >> 7)
         {
             
-            c1 = fgetc(arq_entrada);
+            byte1 = fgetc(arq_entrada);
             
             
-            aux[tm] = c1;
+            palavra[tm] = byte1;
             tm++;
         }
 
-        varint_utf_aux(aux, tm, arq_saida);
+        varint_utf_aux(palavra, tm, arq_saida);
        
         
     }
